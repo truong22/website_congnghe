c52daecb8270197fecf484d437cd28d73aebe1c8
truong <buidinhtruong2201@gmail.com>
2026-04-09 00:20:49 +0700

    Shopconfig cau hinh bao mat

--- src/main/java/com/devteria/hello_spring_boot/Security/Confix/ShopConfig.java
+++ src/main/java/com/devteria/hello_spring_boot/Security/Confix/ShopConfig.java
@@ -1,14 +1,71 @@
 package com.devteria.hello_spring_boot.Security.Confix;
 
+import jakarta.servlet.http.HttpServlet;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//                                                                  ↑ Đúng package này
+import com.devteria.hello_spring_boot.Security.User.shopUserDetailsService;
+import com.devteria.hello_spring_boot.Security.jwt.AuthTokenFilter;
+import com.devteria.hello_spring_boot.Security.jwt.JwtAuthEntryPoint;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.util.List;
+
+@EnableMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+@RequiredArgsConstructor
 @Configuration
 public class ShopConfig {
+    private  final shopUserDetailsService shopUserDetailsService;
+    private final JwtAuthEntryPoint jwtAuthEntryPoint;
+    private static final List<String>SECURED_URLS=List.of("/api/v1/cats/**","/api/v1/cartItems");
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
     }
-
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthTokenFilter authTokenFilter(){
+        return  new AuthTokenFilter();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig)throws Exception{
+        return authConfig.getAuthenticationManager();
+    }
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        var authProvider=new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(shopUserDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exception->exception.authenticationEntryPoint(jwtAuthEntryPoint))
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth->auth.requestMatchers(SECURED_URLS.toArray(String[]::new))
+                .authenticated().anyRequest().permitAll());
+        http.authenticationProvider(daoAuthenticationProvider());
+        http.addFilterBefore(authTokenFilter(),
+                UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
